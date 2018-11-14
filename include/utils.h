9e4470314b22e012fd167650222364cc21b1b68e
solanav <solanav@qq.com>
2018-11-14 18:23:40 +0100

    Added description for reverse shell

--- include/utils.h
+++ include/utils.h
@@ -5,10 +5,22 @@
 
 #define EXIT_COMMAND "exit\n"
 
+/** 
+ * Reverse shell using exec
+ * 
+ * It has the same limitations as exec. You can end the reverse shell by typing exit.
+ * 
+ * ip_addr - String with ip address of the listener
+ * port - int where the port goes.
+ * 
+ * Returns - OK or ERROR
+*/
 int reverse_shell(char *ip_addr, int port);
 
 /** 
  * Wrapper for popen
+ * 
+ * Should be able to execute basic commands on the shell. Same as if you used them directly on popen.
  * 
  * Command - String with the command to execute
  * Output - String where the output of the command is saved. Memory is allocated inside.
