0da953bb52bd85ce6f130e18bf63b44758968a7c
solanav <solanav@qq.com>
2018-11-20 01:38:32 +0100

    Server now listens for UDP pings from clients

--- src/server.c
+++ src/server.c
@@ -4,18 +4,26 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <netinet/in.h>
 
 #define OK 0
 #define ERROR -1
 #define STD_SIZE 512
 
-int create_socket(int port, int *socket_desc, struct sockaddr_in *server);
-int update_client(int *socket_desc, struct sockaddr_in *server);
-int menu(int *socket_desc, struct sockaddr_in *server);
+int create_socket(int port, int *socket_desc, struct sockaddr_in *server,
+		  int isTCP);
+int update_client();
+int listen_client();
+int menu();
 
-int create_socket(int port, int *socket_desc, struct sockaddr_in *server)
+int create_socket(int port, int *socket_desc, struct sockaddr_in *server,
+		  int isTCP)
 {
-	(*socket_desc) = socket(AF_INET, SOCK_STREAM, 0);
+	if (isTCP)
+		(*socket_desc) = socket(AF_INET, SOCK_STREAM, 0);
+	else
+		(*socket_desc) = socket(AF_INET, SOCK_DGRAM, 0);
+
 	if ((*socket_desc) == -1) {
 		printf("Could not create socket");
 	}
@@ -33,18 +41,45 @@ int create_socket(int port, int *socket_desc, struct sockaddr_in *server)
 	return OK;
 }
 
-int update_client(int *socket_desc, struct sockaddr_in *server)
+int listen_client()
 {
+	int socket_desc, client_size, recv_size;
+	struct sockaddr_in server, client;
+	char buffer[STD_SIZE];
+
+	client_size = sizeof(client);
+
+	if (create_socket(8080, &socket_desc, &server, 0) == ERROR)
+		return ERROR;
+
+	while (1) {
+		recv_size = recvfrom(socket_desc, buffer, sizeof(buffer), 0,
+				     (struct sockaddr *)&client, &client_size);
+		printf("[%s:%d] [%s]\n", inet_ntoa(client.sin_addr),
+		       ntohs(client.sin_port), buffer);
+	}
+
+	return OK;
+}
+
+int update_client()
+{
+	int socket_desc;
+	struct sockaddr_in server;
+
 	int new_socket, c;
 	struct sockaddr_in client;
 	char buffer[STD_SIZE];
 	size_t flag = 0;
 	FILE *file_to_send;
 
-	printf("Waiting for connections...\n");
-	listen(*socket_desc, 1);
+	if (create_socket(8080, &socket_desc, &server, 0) == ERROR)
+		return ERROR;
 
-	new_socket = accept(*socket_desc, (struct sockaddr *)&client,
+	printf("Waiting for connections...\n");
+	listen(socket_desc, 1);
+
+	new_socket = accept(socket_desc, (struct sockaddr *)&client,
 			    (socklen_t *)&c);
 
 	file_to_send = fopen("../ezekiel", "r");
@@ -64,12 +99,12 @@ int update_client(int *socket_desc, struct sockaddr_in *server)
 	return 0;
 }
 
-int menu(int *socket_desc, struct sockaddr_in *server)
+int menu()
 {
 	short user_command;
 
 	printf("[0] Update client\n");
-	printf("[1] Nothing\n");
+	printf("[1] Listen for ping\n");
 	printf("> ");
 
 	scanf("%hd", &user_command);
@@ -77,8 +112,13 @@ int menu(int *socket_desc, struct sockaddr_in *server)
 	switch (user_command) {
 	case 0:
 		system("clear");
-		update_client(socket_desc, server);
+		update_client();
 		break;
+	case 1:
+		system("clear");
+		listen_client();
+		break;
+
 	default:
 		printf("Option not found\n");
 	}
@@ -86,11 +126,5 @@ int menu(int *socket_desc, struct sockaddr_in *server)
 
 int main()
 {
-	int socket_desc;
-	struct sockaddr_in server;
-
-	if (create_socket(8080, &socket_desc, &server) == ERROR)
-		return ERROR;
-
-	menu(&socket_desc, &server);
+	menu();
 }
